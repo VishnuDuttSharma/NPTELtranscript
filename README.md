06b29ae80d356033f864f6aa4c99a8d805729d5a
VishnuDuttSharma <vishnusharma619@gmail.com>
2016-02-21 22:25:24 +0530

    Added Database; Improved file handling

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Requirements:
 You need to have Python and BeautifulSoup package installed
 
 Running Instruction:
-When running for the first time, uncomment the 'update' function. It will update the list of courses and links and write them on the disk, to make the process faster. Afterwards you do not need to run 'update'.
 Simply run the program and choose from the lists shown as instructed.
+If the course is available on the website but does not appear in the list, uncomment the 'update' function. It will update the list of courses and links and write them on the disk, to make the process faster. Afterwards you do not need to run 'update'.
--- scrapy.py
+++ scrapy.py
@@ -2,9 +2,9 @@ import urllib2
 from bs4 import BeautifulSoup
 import csv
 import math
+import os
 
 ADDRESS = "http://textofvideo.nptel.iitm.ac.in/"
-DESTINTION = "Download/"
 
 class Branch:
     def __init__( self, branchName, link, numVideo, numDoc, numMP3):
@@ -48,7 +48,7 @@ class Subject:
         #return self.name+'\t'+self.faculty+'\t'+self.institute
         return self.name+'\n     '+self.faculty+'\n     '+self.institute
 
-def updateBranch( data,  fileName = 'Branch_List.csv'):
+def updateBranch( data,  fileName = './Database/Branch_List.csv'):
 
     myfile = open( fileName, 'wb')
 
@@ -77,7 +77,7 @@ def updateBranch( data,  fileName = 'Branch_List.csv'):
 
     myfile.close()
 
-def loadBranchList( fileName = 'Branch_List.csv' ):
+def loadBranchList( fileName = './Database/Branch_List.csv' ):
 
     data = []
     with open( fileName, 'rb') as myfile:
@@ -98,9 +98,9 @@ def getBranchData( address = ADDRESS ):
     table = soup.find_all( 'td', attrs={'bgcolor':'#ebeaea'})
     for i in range(0, len(table)-5, 5):
         #print table[i+1].text,':',table[i + 2].text,':',table[i+3].text,':',table[i+4].text,':',str(address) + str((((table[i+1]).span).a)['href'])
-        b1 = Subject( (table[i+1].text).strip(), str(ADDRESS) + str((((table[i+1]).span).a)['href']), int(table[i + 2].text), int(table[i + 3].text) )
+        b1 = Branch( (table[i+1].text).strip(), str(ADDRESS) + str((((table[i+1]).span).a)['href']), int(table[i + 2].text), int(table[i + 3].text), int(table[i + 4].text) )
         data.append(b1)
-    # print data
+    # print len(data)
     return data
 
 def getSubjectData( address ):
@@ -123,7 +123,7 @@ def updateSubject( branch_data ):
         subject_link =  element.link
         subject_list =  getSubjectData( subject_link )
 
-        myfile = open( element.name +'.csv', 'wb')
+        myfile = open( './Database/'+element.name +'.csv', 'wb')
 
         myfile.write( 'Course' )
         myfile.write( ',' )
@@ -165,14 +165,43 @@ def loadSubjectList( fileName ):
 
 def printList( data ):
     for i in range(0, len(data)):
-        print " %2d. %s" % (i, data[i].getDescription())
+        print " %2d. %s" % (i+1, data[i].getDescription())
+    print "Press 0 for previous menu"
 
 def download_file(download_url, name):
-    response = urllib2.urlopen(download_url)
-    file = open( DESTINTION+name, 'wb')
-    file.write(response.read())
-    file.close()
-    print "Completed ", name
+    try:
+        response = urllib2.urlopen(download_url)
+        file = open( name, 'wb')
+        file.write(response.read())
+        file.close()
+        print "Completed ", (name.split('/'))[-1]
+    except:
+        print 'Could not download ',name
+
+def downloadVideo( url ):
+    # "http://download.thinkbroadband.com/10MB.zip"
+
+    file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+
+    f.close()
 
 def getLectures( address ):
     data = []
@@ -190,8 +219,11 @@ def getLectures( address ):
 
     return data
 
-def getTranscript( address ):
+def getTranscript( address, folder ):
     data = []
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
     # print address
     response = urllib2.urlopen( address )
@@ -210,28 +242,52 @@ def getTranscript( address ):
         data = data + dataPart
 
     for i in range( 0, int(total)):
-        fileName = data[i]+' [lec'+str(i+1)+'].pdf'
-        fileName = fileName.replace('/', '-')
+        fileName = '[lec-'+str(i+1)+'] '+ data[i] +'.pdf'
+        fileName = folder+'/'+fileName.replace('/', '-')
+        print " %2d/%d"  % ( i+1, int(total)),
         download_file( ADDRESS+courseID+'/lec'+str(i+1)+'.pdf',  fileName)
 
 def update( address = ADDRESS):
+
+    # if not os.path.exists('./Download/'):
+    #     os.makedirs('./Download')
+
+    if not os.path.exists('./Database/'):
+        os.makedirs('./Database')
+
+
+    print 'Updating Resources'
     branch_list = getBranchData( ADDRESS )
+    print 'Data Fetched Successfully'
     updateBranch( branch_list )
+    print 'Branch Database Updated'
     updateSubject( branch_list)
+    print 'Updating Complete'
+
 
 def __main__():
 
-    update( ADDRESS )
+    # update( ADDRESS )
 
     branch_list = loadBranchList()
+
     printList( branch_list )
-    option1 = int( raw_input('>> Choose branch(number) from the above list: '))
 
-    fileName = branch_list[ option1 ].name + '.csv'
-    subject_list = loadSubjectList( fileName )
-    printList( subject_list )
-    option2 = int( raw_input('>> Choose subject(number) from the above list: '))
+    while( True):
+        option1 = int( raw_input('>> Choose branch(number) from the above list: '))
 
-    getTranscript( subject_list[ option2 ].link )
+        if( option1 == 0):
+            continue
+
+        fileName = './Database/'+branch_list[ option1 - 1].name + '.csv'
+
+        subject_list = loadSubjectList( fileName )
+        printList( subject_list )
+        option2 = int( raw_input('>> Choose subject(number) from the above list: '))
+
+        if( option2 != 0):
+            break
+
+    getTranscript( subject_list[ option2 - 1 ].link, subject_list[ option2 - 1].name )
 
 __main__()
